a3a129cdd6a7e5dfe74f96baa3869a30e24090a8
Braxton Fair <hello@taxborn.com>
2023-05-28 03:10:50 -0500

    convert to hex and optimize

--- src/u8read.c
+++ src/u8read.c
@@ -5,12 +5,13 @@
 #include "u8read.h"
 
 static uint8_t codepoint_bytes(uint8_t input) {
+    // first check if we are multi-byte at all, if not, just a single byte
+    // code point. this just checks if the MSB is 1.
+    if (input >> 7 == 0) return 1;
+
     // we only really care about the top 4 bits of the input, so
     // shift them down to work with them
     input >>= 4;
-    // first check if we are multi-byte at all, if not, just a single byte
-    // code point. this just checks if the MSB is 1.
-    if (input >> 3 == 0) return 1;
 
     int count = 0;
 
@@ -26,29 +27,27 @@ static uint8_t codepoint_bytes(uint8_t input) {
 }
 
 static uint32_t compute_codepoint(char *buffer, int index, int length) {
-    if (length == 1) {
-        return   ((buffer[index]     & 0x7F) << 0);
-    }
-    if (length == 2) {
-        return   ((buffer[index]     & 0x1F) << 6)
-               | ((buffer[index + 1] & 0x3F));
-    }
-    if (length == 3) {
-        return   ((buffer[index]     & 0x0F) << 12)
-               | ((buffer[index + 1] & 0x3F) << 6)
-               | ((buffer[index + 2] & 0x3F));
-    }
-    if (length == 4) {
-        return   ((buffer[index]     & 0x07) << 18)
-               | ((buffer[index + 1] & 0x3F) << 12)
-               | ((buffer[index + 2] & 0x3F) << 6)
-               | ((buffer[index + 3] & 0x3F));
-    }
+    switch (length) {
+        case 1:
+            return   (buffer[index]     & 0x7F) << 0;
+        case 2:
+            return   (buffer[index]     & 0x1F) << 6
+                   | (buffer[index + 1] & 0x3F);
+        case 3:
+            return   (buffer[index]     & 0x0F) << 12
+                   | (buffer[index + 1] & 0x3F) << 6
+                   | (buffer[index + 2] & 0x3F);
+        case 4:
+            return   (buffer[index]     & 0x07) << 18
+                   | (buffer[index + 1] & 0x3F) << 12
+                   | (buffer[index + 2] & 0x3F) << 6
+                   | (buffer[index + 3] & 0x3F);
+        default:
+            // if we don't get one of those byte lengths, there's an issue.
+            printf("ERROR IN DECODING (bytes = %d). expected at most 4.\n", length);
 
-    // if we don't get one of those byte lengths, there's an issue.
-    printf("ERROR IN DECODING (bytes = %d). expected at most 4.\n", length);
-
-    exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE);
+    }
 }
 
 void print_codepoints(char* buffer) {
@@ -56,7 +55,7 @@ void print_codepoints(char* buffer) {
 
     while (buffer[index] != '\0') {
         // check if we indexed into the middle of a codepoint
-        if ((buffer[index] & 0b11000000) == 0b10000000) {
+        if ((buffer[index] & 0xc0) == 0x80) {
             // TODO if we do this, we can backtrack until we found a valid
             printf("ERROR: indexed the middle of a codepoint. advancing...\n");
             index += 1;
@@ -66,7 +65,7 @@ void print_codepoints(char* buffer) {
         int bytes = codepoint_bytes(buffer[index]);
         uint32_t codepoint = compute_codepoint(buffer, index, bytes);
 
-        printf("U+%x (bytes = %d): '%lc'\n", codepoint, bytes, codepoint);
+        printf("U%x: '%lc' (%d bytes)\n", codepoint, codepoint, bytes);
 
         index += bytes;
     }
