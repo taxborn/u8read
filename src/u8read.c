edb8720ddee69fa8440e8cf2203c5fc6a5361170
Braxton Fair <hello@taxborn.com>
2023-05-28 08:34:26 -0500

    clean up code and improve comments

--- src/u8read.c
+++ src/u8read.c
@@ -7,59 +7,42 @@
 #include "u8read.h"
 
 // check to see if the input is a valid unicode byte 1 format.
-static bool is_valid_unicode_start(uint8_t input) {
+static uint8_t unicode_codepoint_length(uint8_t input) {
     // 1-byte case, 0b0xxx'xxxx
-    if ((input & 0x80) == 0x00) return true;
+    if ((input & 0x80) == 0x00) return 1;
     // 2-byte case, 0b110x'xxxx
-    if ((input & 0xE0) == 0xC0) return true;
+    if ((input & 0xE0) == 0xC0) return 2;
     // 3-byte case, 0b1110'xxxx
-    if ((input & 0xF0) == 0xE0) return true;
+    if ((input & 0xF0) == 0xE0) return 3;
     // 4-byte case, 0b1111'0xxx
-    if ((input & 0xF8) == 0xF0) return true;
+    if ((input & 0xF8) == 0xF0) return 4;
 
-    return false;
-}
-
-// count the number of bytes the unicode codepoint should be
-static uint8_t codepoint_bytes(uint8_t input) {
-    // we have already checked if we have indexed into a middle-byte so we shouldn't 
-    // hit this, but might be good to check.
-    assert(is_valid_unicode_start(input) && "Can only calculate codepoint bytes from a valid UTF-8 byte 1 format.");
-
-    // first check if we are multi-byte at all, if not, just a single byte
-    // code point. this just checks if the MSB is 1.
-    if (input >> 7 == 0) return 1;
-
-    // we only really care about the top 4 bits of the input, so
-    // shift them down to work with them
-    input >>= 4;
-
-    int count = 0;
-
-    // loop to count the number of set bits in an input
-    for (; input; input >>= 1) count += input & 1;
-
-    // the number of ones is equal to how many bytes the codepoint has
-    return count;
+    // not valid
+    return 0;
 }
 
 // compute the 32-bit codepoint
-static uint32_t compute_codepoint(char *buffer, int index, int length) {
+static uint32_t unicode_codepoint_of(char *buffer, int index, int length) {
     switch (length) {
+        // 0b0xxxxxxx [U+0000 - U+007F]
         case 1:
             return   (buffer[index]     & 0x7F) << 0;
+        // 0b110xxxxx 0b10xxxxxx [U+0080 - U+07FF]
         case 2:
             return   (buffer[index]     & 0x1F) << 6
                    | (buffer[index + 1] & 0x3F);
+        // 0b1110xxxx 0b10xxxxxx 0b10xxxxxx [U+0800 - U+FFFF]
         case 3:
             return   (buffer[index]     & 0x0F) << 12
                    | (buffer[index + 1] & 0x3F) << 6
                    | (buffer[index + 2] & 0x3F);
+        // 0b11110xxx 0b10xxxxxx 0b10xxxxxx 0b10xxxxxx [U+10000 - U+10FFFF]
         case 4:
             return   (buffer[index]     & 0x07) << 18
                    | (buffer[index + 1] & 0x3F) << 12
                    | (buffer[index + 2] & 0x3F) << 6
                    | (buffer[index + 3] & 0x3F);
+        // we should not get here, but exit if we do.
         default:
             // if we don't get one of those byte lengths, there's an issue.
             printf("ERROR IN DECODING (bytes = %d). expected at most 4.\n", length);
@@ -92,8 +75,12 @@ void print_codepoints(char* buffer) {
             continue;
         }
 
-        int bytes = codepoint_bytes(byte);
-        uint32_t codepoint = compute_codepoint(buffer, index, bytes);
+        int bytes = unicode_codepoint_length(byte);
+        if (bytes == 0) {
+            printf("unable to determine length of codepoint: %.8b", byte);
+            exit(EXIT_FAILURE);
+        }
+        uint32_t codepoint = unicode_codepoint_of(buffer, index, bytes);
 
         if (bytes <= 3) {
             printf("U-%.4X: '%lc' (%d bytes)\n", codepoint, codepoint, bytes);
